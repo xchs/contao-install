ceed01f03f54cd87d7104d23118a8d57a460e558
xchs <github@gmx.net>
2012-11-17 16:48:36 +0100

    A few little changes & code formatting

--- contao-install.php
+++ contao-install.php
@@ -1,68 +1,75 @@
 <?php
 
-	/**
-	 * Contao Open Source CMS
-	 *
-	 * Contao Download Script
-	 * Gets the current stable Contao release and runs the Contao install tool
-	 * Gets a certain GitHub repository branch if specified as URL parameter
-	 *
-	 * @package   Contao
-	 * @link      http://git.io/contao-core
-	 * @author    xchs <http://git.io/xchs>
-	 * @copyright xchs 2012
-	 */
+/**
+ * Contao Open Source CMS
+ *
+ * Contao Download Script
+ * Gets the current stable Contao release and runs the Contao install tool
+ * Gets a certain GitHub repository branch if specified as URL parameter
+ *
+ * @package   Contao
+ * @link      http://git.io/contao-core
+ * @author    xchs <http://git.io/xchs>
+ * @copyright xchs 2012
+ */
 
-	// Check for a given URL parameter to switch between GitHub repository branches
-	if (isset($_SERVER['QUERY_STRING']))
+// Check for a given URL parameter to switch between GitHub repository branches
+if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != '')
+{
+	// Sanatize for security, remove anything but 0-9,a-z,-_,
+	$strBranch = preg_replace("/[^0-9a-z\-_,]+/i", "", $_SERVER['QUERY_STRING']);
+
+	// Get the respective repository branch from GitHub
+	switch ($strBranch)
 	{
-		// Sanatize for security, remove anything but 0-9,a-z,-_,
-		$strBranch = preg_replace("/[^0-9a-z\-_,]+/i", "", $_SERVER['QUERY_STRING']);
+		case 'master':
+			// Get the current "master" branch from the GitHub repository
+			shell_exec("curl -s -L https://github.com/contao/core/tarball/master | tar -xzp");
+			break;
 
-		// Get the respective repository branch from GitHub
-		switch ($strBranch)
-		{
-			case 'master':
-				shell_exec("curl -s -L https://github.com/contao/core/tarball/master | tar -xzp");
-				break;
+		case 'lts':
+			// Get the current "lts" branch from the GitHub repository
+			shell_exec("curl -s -L https://github.com/contao/core/tarball/lts | tar -xzp");
+			break;
 
-			case 'lts':
-				shell_exec("curl -s -L https://github.com/contao/core/tarball/lts | tar -xzp");
-				break;
-
-			case 'develop':
-				shell_exec("curl -s -L https://github.com/contao/core/tarball/develop | tar -xzp");
-				break;
-		}
-	}
-	else
-	{
-		// Get the current stable release from SourceForge (http://sourceforge.net/projects/contao/files/latest/download)
-		shell_exec("curl -s -L http://install.contao.org | tar -xzp");
+		case 'develop':
+			// Get the current "develop" branch from the GitHub repository
+			shell_exec("curl -s -L https://github.com/contao/core/tarball/develop | tar -xzp");
+			break;
 	}
 
 	// Save the subfolder name of the unzip directory
-	$folder = trim(shell_exec("ls -d contao-*"));
+	$folder = trim(shell_exec("ls -d contao-core-*"));
+}
+else
+{
+	// Get the current stable release from SourceForge (http://sourceforge.net/projects/contao/files/latest/download)
+	shell_exec("curl -s -L http://install.contao.org | tar -xzp");
 
-	// Remove some unwanted files and folders
-	// shell_exec("rm -rf $folder/.gitignore $folder/README.md $folder/.tx");
+	// Save the subfolder name of the unzip directory
+	$folder = trim(shell_exec("ls -d contao-[0-9]*"));
+}
 
-	// Move subfolder contents into current directory
-	shell_exec("mv $folder/* ./");
 
-	// Make sure to move also hidden files and folders
-	shell_exec("mv $folder/.[a-z]* ./");
+// Remove some unwanted files and folders
+// shell_exec("rm -rf $folder/.gitignore $folder/README.md $folder/.tx");
 
-	// Enable the ".htaccess" file
-	shell_exec("cp .htaccess.default .htaccess");
+// Move subfolder contents into current directory
+shell_exec("mv $folder/* ./");
 
-	// Remove the unzip directory
-	shell_exec("rm -rf $folder");
+// Make sure to move also hidden files and folders
+shell_exec("mv $folder/.[a-z]* ./");
 
-	// Remove the download script
-	shell_exec("rm contao-install.php");
+// Enable the ".htaccess" file
+shell_exec("cp .htaccess.default .htaccess");
 
-	// Redirect to the Contao install tool
-	Header("Location: contao/install.php");
+// Remove the unzip directory
+shell_exec("rm -rf $folder");
+
+// Remove the download script
+shell_exec("rm contao-install.php");
+
+// Redirect to the Contao install tool
+Header("Location: contao/install.php");
 
 ?>
