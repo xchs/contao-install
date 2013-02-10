8ac56e2fc8c57d7c7f17496b1bcf2f08b0875b6c
xchs <github@gmx.net>
2013-02-10 20:14:43 +0100

    Add support for a more general branch naming scheme

--- contao-install.php
+++ contao-install.php
@@ -24,27 +24,11 @@ if (isset($argv[1]) || (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRIN
 	else
 	{
 		// Sanatize for security, remove anything but 0-9,a-z,-_,
-		$strBranch = preg_replace("/[^0-9a-z\-_,]+/i", "", $_SERVER['QUERY_STRING']);
+		$strBranch = preg_replace("/[^0-9a-z\-_,.\/]+/i", "", $_SERVER['QUERY_STRING']);
 	}
 
 	// Get the respective repository branch from GitHub
-	switch ($strBranch)
-	{
-		case 'master':
-			// Get the current "master" branch from the GitHub repository and extract the tar archive (tarball)
-			shell_exec("curl -s -L https://github.com/contao/core/tarball/master | tar -xzp");
-			break;
-
-		case 'lts':
-			// Get the current "lts" branch from the GitHub repository and extract the tar archive (tarball)
-			shell_exec("curl -s -L https://github.com/contao/core/tarball/lts | tar -xzp");
-			break;
-
-		case 'develop':
-			// Get the current "develop" branch from the GitHub repository and extract the tar archive (tarball)
-			shell_exec("curl -s -L https://github.com/contao/core/tarball/develop | tar -xzp");
-			break;
-	}
+	shell_exec("curl -s -L https://github.com/contao/core/tarball/" . $strBranch . " | tar -xzp");
 
 	// Save the subfolder name of the unzip directory
 	$folder = trim(shell_exec("ls -d contao-core-*"));
